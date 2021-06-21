2e79a8d3ac8036326481ba567a53074e4a468f7b
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-06-21 21:08:21 +0800

    Add files via upload

---
+++ 9.赋值运算符.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//¸³ÖµÔËËã·û
+
+	//=
+	int a = 10;
+	a = 100;
+	cout <<  "a = " << a << endl;
+
+
+	//+=
+	a = 10;
+	a += 2; //a = a + 2;
+	cout << "a = " << a << endl;
+
+	//-=
+	a = 10;
+	a -= 2;
+	cout << "a = " << a << endl;
+
+	//*=
+	a = 10;
+	a *= 2;// a = a - 2;
+	cout << "a = " << a << endl;
+
+	// /=
+	a = 10;
+	a /= 2; //a = a / 2
+	cout << "a = " << a << endl;
+
+	// %=
+	a = 10;
+	a %= 2;
+    cout << "a = " << a << endl;
+
+	system("pause");
+
+	return 0;
+}
