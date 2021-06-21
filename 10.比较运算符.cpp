89359af4a0d704cbd798360557f7dab0e353d9f1
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-06-21 21:08:35 +0800

    Add files via upload

---
+++ 10.比较运算符.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//±È½ÏÔËËã·û
+
+	//==
+	int a = 10;
+	int b = 20;
+
+	cout << (a == b) << endl;
+
+	//!=
+	cout << (a != b) << endl;	
+
+	//>
+	cout << (a > b) << endl;
+
+	//<
+	cout << (a < b) << endl;
+
+	//>=
+	cout << (a >= b) << endl;
+
+	//<=
+	cout << (a <= b) << endl;
+
+	system("pause");
+
+	return 0;
+
+}
