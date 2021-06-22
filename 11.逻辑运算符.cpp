5b1b463392b0b9d0593e8fc7c88916ea55b4e1d3
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-06-22 14:18:00 +0800

    Add files via upload

---
+++ 11.逻辑运算符.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Âß¼­ÔËËã·û ·Ç!
+	int a = 10;
+	cout << !a << endl;
+
+	int b = 10;
+	cout << (a && b) << endl;
+
+
+	system("pause");
+
+	return 0;
+}
