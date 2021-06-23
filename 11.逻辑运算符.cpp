9dad66c32872a6f914ee7d2eda63df38ce88f2e4
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-06-23 08:21:08 +0800

    Add files via upload

--- 11.逻辑运算符.cpp
+++ 11.逻辑运算符.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 int main()
 {
-	//逻辑运算符 非!
+	/*//逻辑运算符 非!
 	int a = 10;
 	cout << !a << endl;
 
 	int b = 10;
-	cout << (a && b) << endl;
+	cout << (a && b) << endl;*/
+
+	//逻辑运算符 或||
+	int a = 10;
+	int b = 10;
+
+	cout << (a || b) << endl;
+	//逻辑或 “同假为假，其他为真”
 
 
 	system("pause");
