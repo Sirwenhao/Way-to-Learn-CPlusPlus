d1e015af512e09efc169a939c3a0346868a89872
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-06-26 11:26:53 +0800

    Add files via upload

---
+++ 19.while循环语句.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//while循环
+	// 在屏幕上打印0~9十个数字
+
+	int num = 0;
+	//cin >> num;
+	while (num < 10)
+	{
+	
+		cout << num << endl;
+		num++;
+	}
+
+	system("pause");
+
+	return 0;
+}
